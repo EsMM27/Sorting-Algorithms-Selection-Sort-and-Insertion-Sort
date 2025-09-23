d5a7a5680d0d257f7cf22723df8173165687dbd9
Edgar <44414317+EsMM27@users.noreply.github.com>
2025-09-23 19:05:03 +0100

    binarySearchTree

---
+++ Tree/Person.java
@@ -0,0 +1,27 @@
+package Tree;
+
+public class Person implements Comparable<Person> {
+    
+    private String firstName;
+    private String lastName;
+    private int age = 0;
+
+    @Override
+    public int compareTo(Person other) {
+        return this.lastName.compareTo(other.lastName);
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " (" + age + ")";
+    }
+
+
+
+}
